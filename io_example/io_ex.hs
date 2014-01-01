290292db78f793429046c7bd0ebcad47ba0fc6e3
John May <johnmay10000@gmail.com>
2014-01-01 17:41:44 +1000

    Added io example

---
+++ io_example/io_ex.hs
@@ -0,0 +1,43 @@
+import System.Environment
+import Control.Applicative
+import Control.Monad
+
+{-
+
+Functions --
+	getArgs :: IO [String]
+	putStrLn :: IO String
+	readFile :: filePath -> IO String
+	lines :: String -> [String]
+    void :: IO a -> IO ()  
+
+	abstractions --
+			Applicative, Monad
+		(<$>) :: (a -> b) -> IO a -> IO b
+		(<*>) :: IO (a -> b) -> IO a -> IO b
+		(>>=) :: IO a -> (a -> IO b) -> IO b
+		(=<<) :: (a -> IO b) -> IO a -> IO b
+		pure :: a -> IO a
+-}
+
+-- Hint getArgs and run
+{- 
+
+Hint : use getFiles and printFiles
+
+run :: String -> IO () 
+run = undefined
+
+getFiles :: [Strings] -> IO [(String, String)]
+getFiles = undefined
+
+getFile :: String -> IO (String,String)
+getFile = undefined
+
+printFiles :: [(String, String)] -> IO ()
+-}
+
+
+
+	
+	 
